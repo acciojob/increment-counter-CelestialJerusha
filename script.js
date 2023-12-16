2c49c1e274618754c96ba7920fa092291bee99cf
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-12-17 00:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+function increase(){
+	const counter = document.getElementById("counter");
+	counter.innerText = parseInt(counter.innerText)+1;
+	alert(parseInt(counter.innerText))
+	// counter.innerText += parseInt(counter)+1;
+}
